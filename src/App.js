5e9fe059ffcb63d0143ba8fba445d424bbd3c98d
Chris Nyeche <91467592+chrisdesignsofficial@users.noreply.github.com>
2022-09-22 09:14:34 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Header from "./Components/Header"
-import Hero from "./Components/Hero"
-import Features from "./Components/Features";
-import Testimonials from "./Components/Testimonials";
-import Footer from "./Components/Footer";
-
+import Hero from "./Components/Hero";
 function App() {
   return (
+
     <>
-    <Header/>
-    <Hero/>
-    <Features/>
-    <Testimonials/>
-    <Footer/>
+      <Hero />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import './index.css';
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+// Boostrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-// Animate on scroll
+import "./index.css";
+
+// AOS
 import "aos/dist/aos.css";
 
-// ChakraProvider` component
+//import animate
+import 'animate.css';
+
+// `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -25,4 +30,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
